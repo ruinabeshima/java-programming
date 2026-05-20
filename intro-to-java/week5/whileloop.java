10ba1c606daf0d2e88f16e4fb685dfc46d807df5
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-05-20 19:16:57 +0900

    simple while loop

---
+++ intro-to-java/week5/whileloop.java
@@ -0,0 +1,14 @@
+/*
+Simple while loop
+ */
+
+public class whileloop {
+  public static void main(String args[]) {
+    int i = 0;
+    while (i < 10) {
+      System.out.println("Number: " + i);
+      i += 1;
+    }
+    System.out.println("Finish");
+  }
+}

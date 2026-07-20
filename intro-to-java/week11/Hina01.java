6047c786c0f58927f3812b0b911410d1f562422a
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-07-20 11:08:11 +0900

    fix

--- intro-to-java/week11/Hina01.java
+++ intro-to-java/week11/Hina01.java
@@ -1,31 +1,27 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 public class Hina01 extends JFrame {
 
-    public Hina01() {
-        setSize(800, 500);
-        setTitle("Java Programing");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        MyJPanel myJPanel = new MyJPanel();
-        Container c = getContentPane();
-        c.add(myJPanel);
-        setVisible(true);
-    }
+  public Hina01() {
+    setSize(800, 500);
+    setTitle("Java Programing");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    MyJPanel myJPanel = new MyJPanel();
+    Container c = getContentPane();
+    c.add(myJPanel);
+    setVisible(true);
+  }
 
-    public static void main(String args[]) {
-        new Hina01();
-    }
+  public static void main(String args[]) { new Hina01(); }
 
-    public class MyJPanel extends JPanel {
-        public MyJPanel() {
-            setBackground(Color.WHITE);
-        }
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawOval(100, 100, 200, 200);  // 楕円を描く / Draw an oval (outline)
-            g.fillOval(300, 100, 400, 200);  // 塗りつぶした楕円を描く / Draw a filled oval
-        }
+  public class MyJPanel extends JPanel {
+    public MyJPanel() { setBackground(Color.WHITE); }
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      g.drawOval(100, 100, 200, 200); // 楕円を描く / Draw an oval (outline)
+      g.fillOval(300, 100, 400,
+                 200); // 塗りつぶした楕円を描く / Draw a filled oval
     }
+  }
 }
